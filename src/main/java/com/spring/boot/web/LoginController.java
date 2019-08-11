12bf6d5745adfa3e6b4c9cf0db21f629715daee7
wuzhendong123 <34954340+wuzhendong123@users.noreply.github.com>
2019-08-11 23:24:07 +0800

    test

--- src/main/java/com/spring/boot/web/LoginController.java
+++ src/main/java/com/spring/boot/web/LoginController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.spring.boot.entity.Operator;
+import com.spring.boot.util.ServletUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,12 +15,25 @@ public class LoginController {
 	public String toLogin() {
 		return "login/login";
 	}
-	@RequestMapping("/login")	
+/*	@RequestMapping("/login")
 	public String login(@RequestParam("name") String name,@RequestParam("passWord")String passWord,HttpServletRequest httpRequest) {
 		HttpSession session=httpRequest.getSession();
 		Operator operator=new Operator();
 		operator.setName(name);
 		session.setAttribute("auth", operator);
 		return "redirect:index";
+	}*/
+
+	@RequestMapping("/login")
+	public String login(@RequestParam("name") String name,@RequestParam("passWord")String passWord,HttpServletRequest httpRequest) {
+		// 如果是Ajax请求，返回Json字符串。
+		if (ServletUtils.isAjaxRequest(httpRequest))
+		{
+			return ServletUtils.renderString(response, "{\"code\":\"1\",\"msg\":\"未登录或登录超时。请重新登录\"}");
+		}
+
+		return "login";
 	}
+
+
 }

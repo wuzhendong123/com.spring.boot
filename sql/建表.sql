c0e4f4fbe3d927075e4352aebd0fcfb7f2777f4e
Administrator <630015359@qq.com>
2019-09-10 00:14:04 +0800

    增加历史注解

---
+++ sql/建表.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS `dict_type_history`;
+CREATE TABLE `dict_type_history` (
+  `code` varchar(100) NOT NULL COMMENT '字典主键',
+  `optimistic` int(11) DEFAULT NULL,
+  `dict_name` varchar(100) DEFAULT '' COMMENT '字典名称',
+  `dict_type` varchar(100) DEFAULT '' COMMENT '字典类型',
+  `status` varchar(5) DEFAULT '0' COMMENT '状态（0正常 1停用）',
+  `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='字典类型表';

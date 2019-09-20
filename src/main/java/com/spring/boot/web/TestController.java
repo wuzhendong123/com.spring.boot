140bc99df3c76894f158c2c831a6fac556555531
wuzhendong123 <34954340+wuzhendong123@users.noreply.github.com>
2019-09-20 19:07:14 +0800

    test

--- src/main/java/com/spring/boot/web/TestController.java
+++ src/main/java/com/spring/boot/web/TestController.java
@@ -29,6 +29,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.spring.boot.config.mq.MqProperties;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.util.Collection;
@@ -51,8 +52,16 @@ private CacheManager cacheManager;
     private TempBatchService tempBatchService;
 
 private String prefix = "test/";
+
+
 @Autowired
 private FunctionRuleJudgeMapper functionRuleJudgeMapper;
+
+	@RequestMapping(value="/html")
+public void html(HttpServletResponse response) throws IOException {
+		String html="<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/></head><body><form id = \"pay_form\" action=\"https://gateway.95516.com/gateway/api/frontTransReq.do\" method=\"post\"><input type=\"hidden\" name=\"bizType\" id=\"bizType\" value=\"000902\"/><input type=\"hidden\" name=\"tokenPayData\" id=\"tokenPayData\" value=\"{trId=62000001143&tokenType=01}\"/><input type=\"hidden\" name=\"txnSubType\" id=\"txnSubType\" value=\"00\"/><input type=\"hidden\" name=\"orderId\" id=\"orderId\" value=\"BN2B3L4J2L8GEDGXMLEG\"/><input type=\"hidden\" name=\"backUrl\" id=\"backUrl\" value=\"https://jh.cardinfo.com.cn/payinterface-dispatcher-front/UNIONPAY110001Notice/openCardNotify.htm\"/><input type=\"hidden\" name=\"signature\" id=\"signature\" value=\"Lu9/sJCqQJJxCJqwFxu6xcttY8Aax7xr+GP7aAOi9dEXax3QOOwsQccPCQOH667Hoo+02vjW+Q6OOc02brTHkLtuyvG/mpeQzz/Kt6AxLauKp42tI/eGJdl8Pwc0+XBaU8n9ArMIylgA7yU5mb10V5q/c2KOmhgUpbeuaN3CvKT18G5+ksj0SmisBxWqHYlyAN2LITDDoxzRL4dLbweVUsi8qLO/JAQXsZ7gFOddlv1kIgonugqUzquSm0aMtQhUync+6NuG6Gdoc3kUXkX/2XBSCNpPgPAw0cGp/jeHdaD3vU7CwyRfefOup22uboRCNxpt2e52c970uvtPRElvuw==\"/><input type=\"hidden\" name=\"accNo\" id=\"accNo\" value=\"W5PDNGVA6T8TvU6SdM+g8Q8L4Ik42IHi1XZUx22RmLccedsGX78bZWM6RJzlDpMhZTa8Sm24/YrHWOfdTQdHdTP/jkfYOvB7HJogVIrZqOT/jMz3zBr1QRM+PrJdlEJuOFhrKWy/u/Q5b8/a3paR1NBRdZraQPJC3ayl4Ydhgwxv8waDwVjkHACIVyaXkl21+jy6UVhjF9RL+xLYzgO9VE+I2XW+PhNeJDhKJN8lrixaZcFAld8/u2W1B9+cnCGYQW56/CKOLU5Ynqd/X63PzNACpWftt5e2BZXfeClBJ6RmWbj1hO1NdZnIxY/7vKS9ISkYy8cEb/ex0+9cHZDtlA==\"/><input type=\"hidden\" name=\"customerInfo\" id=\"customerInfo\" value=\"e2NlcnRpZklkPTM1MDMwMTE5OTMwMTEyMjgzMSZjZXJ0aWZUcD0wMSZlbmNyeXB0ZWRJbmZvPUxvSjdrRFNzeURxTXlBekZsVWEvR1ZuZVI1SzlCMFgyWGFGdEoyNEZBbWFMSS9ycm55SllTT0NudDF5TjhVdFdsQWJKTEFPNGZ5SDU1TDVIQnN4cVhWcDM3dWdQWGRVbjExNmlKZ3phTWI2Nm5CbUd0ZUNTRTd2U2IxUDNlQXE1ejU1TzBoMG5XYVJMdFV1VWVnK3lxaE56dEoxMkUrd29jVDAvMFY4czkxMENSV1dUZDRtYzI3UloxVWs0T2U3djU5VllKdFBkZXRhYUsreVpvYmNtNTc1QjdVNThNaWoweFFqNW5lRDRncDFlSHJKb1lTUEg1NFRJZ0xTOHIzNEI5UlZOUG5SMDBMWEI4bmdubFpWdEhGQk5VYmNkcFF0VU82WGNIeTc3cjA5RWZPVGxTdXVXYTlHaHoyUVkxQlhjWVRLOVZRV1pDOVZaWStpZXBpY2MxQT09fQ==\"/><input type=\"hidden\" name=\"txnType\" id=\"txnType\" value=\"79\"/><input type=\"hidden\" name=\"channelType\" id=\"channelType\" value=\"07\"/><input type=\"hidden\" name=\"frontUrl\" id=\"frontUrl\" value=\"https://jh.cardinfo.com.cn/payinterface-dispatcher-front/UNIONPAY110001Notice/frontRcvResponse.htm?orderCode=BN2B3L4J2L8GEDGXMLEG\"/><input type=\"hidden\" name=\"certId\" id=\"certId\" value=\"74215158628\"/><input type=\"hidden\" name=\"encoding\" id=\"encoding\" value=\"UTF-8\"/><input type=\"hidden\" name=\"version\" id=\"version\" value=\"5.1.0\"/><input type=\"hidden\" name=\"accessType\" id=\"accessType\" value=\"0\"/><input type=\"hidden\" name=\"encryptCertId\" id=\"encryptCertId\" value=\"77447321186\"/><input type=\"hidden\" name=\"reqReserved\" id=\"reqReserved\" value=\"BN2B3L4J2L8GEDGXMLEG\"/><input type=\"hidden\" name=\"txnTime\" id=\"txnTime\" value=\"20190920190240\"/><input type=\"hidden\" name=\"merId\" id=\"merId\" value=\"801100048160009\"/><input type=\"hidden\" name=\"accType\" id=\"accType\" value=\"01\"/><input type=\"hidden\" name=\"signMethod\" id=\"signMethod\" value=\"01\"/></form></body><script type=\"text/javascript\">document.all.pay_form.submit();</script></html>";
+		response.getOutputStream().write(html.getBytes());
+}
     @RequestMapping(value="/index")
     public String index() {
        return prefix+"test";
